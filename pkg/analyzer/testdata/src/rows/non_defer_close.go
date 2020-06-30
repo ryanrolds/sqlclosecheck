ee8984c362e01fb2cdb37880188d63e1856abd65
Ryan Olds <ryanrolds@gmail.com>
2020-06-29 18:30:45 -0700

    Check for undefered closes

---
+++ pkg/analyzer/testdata/src/rows/non_defer_close.go
@@ -0,0 +1,31 @@
+package rows
+
+import (
+	"log"
+	"strings"
+)
+
+func non_defer_close() {
+	age := 27
+	rows, err := db.QueryContext(ctx, "SELECT name FROM users WHERE age=?", age)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	names := make([]string, 0)
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatal(err)
+		}
+		names = append(names, name)
+	}
+
+	// Check for errors from iterating over rows.
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%s are %d years old", strings.Join(names, ", "), age)
+
+	rows.Close() // want "Close should use defer"
+}
